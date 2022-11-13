1202995272b303f04c4dc12c8716db4b4899aad8
Jules <juls.gumenna@gmail.com>
2022-11-13 23:46:37 +0200

    added api integration

--- src/app.js
+++ src/app.js
@@ -21,29 +21,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `    
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `    
         <div class="col-2">
-            <div class="wether-forecase-date">${day}</div>
+            <div class="wether-forecase-date">${formatDay(forecastDay.dt)}</div>
             <img
-                src="https://ssl.gstatic.com/onebox/weather/48/rain.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
-                width="42"
+                width="50"
             />
             <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">18</span>
-                <span class="weather-forecast-temperature-min">12</span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
             </div>
         </div>  
     `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
